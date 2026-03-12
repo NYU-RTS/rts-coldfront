78cbbfece8f2c03873a9864c28d95c671103fce9
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-03-12 13:10:37 -0400

    wip impl.
    	new file:   coldfront/core/user/management/commands/pi_grants_publications_request.py
    	new file:   coldfront/templates/email/request_to_add_grants.txt
    	new file:   coldfront/templates/email/request_to_add_publications.txt

---
+++ coldfront/core/user/management/commands/pi_grants_publications_request.py
@@ -0,0 +1,43 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from coldfront.core.grant.models import Grant
+from coldfront.core.project.models import Project
+from coldfront.core.publication.models import Publication
+from coldfront.core.user.models import UserProfile
+from coldfront.core.utils.common import import_from_settings
+
+logger = logging.getLogger(__name__)
+
+GENERAL_RESOURCE_NAME = import_from_settings("GENERAL_RESOURCE_NAME")
+CENTER_BASE_URL = import_from_settings("CENTER_BASE_URL")
+EMAIL_SIGNATURE = import_from_settings("EMAIL_SIGNATURE")
+
+
+class Command(BaseCommand):
+    help = "Send emails to PIs with projects that have no grants/publications associated with them"
+
+    def handle(self, *args, **options):
+        try:
+            for pi_profile in UserProfile.objects.filter(is_pi=True):
+                logger.info(f"processing projects for PI: {pi_profile.user.username}")
+                projects_for_pi = Project.objects.filter(pi=pi_profile)
+                for project in projects_for_pi:
+                    logger.info(f"processing project: {project}")
+                    if Grant.objects.filter(project=project).count() == 0:
+                        grant_notification_context = {
+                            "pi_first_name": pi_profile.user.first_name,
+                            "project": project.title,
+                            "signature": EMAIL_SIGNATURE,
+                        }
+                        logger.info(f"will send email with context: {grant_notification_context}")
+                    if Publication.objects.filter(project=project).count() == 0:
+                        publication_notification_context = {
+                            "pi_first_name": pi_profile.user.first_name,
+                            "project": project.title,
+                            "signature": EMAIL_SIGNATURE,
+                        }
+                        logger.info(f"will send email with context: {publication_notification_context}")
+        except Exception:
+            logger.exception("Exception occured with traceback:", exc_info=True)
