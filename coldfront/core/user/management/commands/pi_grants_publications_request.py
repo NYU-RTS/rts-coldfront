f9b00bfa91d5445e6738a9f54c705774f401b366
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-03-12 16:28:10 -0400

    wip

--- coldfront/core/user/management/commands/pi_grants_publications_request.py
+++ coldfront/core/user/management/commands/pi_grants_publications_request.py
@@ -7,12 +7,14 @@ from coldfront.core.project.models import Project
 from coldfront.core.publication.models import Publication
 from coldfront.core.user.models import UserProfile
 from coldfront.core.utils.common import import_from_settings
+from coldfront.core.utils.mail import send_email_template
 
 logger = logging.getLogger(__name__)
 
 GENERAL_RESOURCE_NAME = import_from_settings("GENERAL_RESOURCE_NAME")
 CENTER_BASE_URL = import_from_settings("CENTER_BASE_URL")
 EMAIL_SIGNATURE = import_from_settings("EMAIL_SIGNATURE")
+EMAIL_SENDER = import_from_settings("EMAIL_SENDER")
 
 
 class Command(BaseCommand):
@@ -31,13 +33,28 @@ class Command(BaseCommand):
                             "project": project.title,
                             "signature": EMAIL_SIGNATURE,
                         }
-                        logger.info(f"will send email with context: {grant_notification_context}")
+                        logger.info(f"sending grant nudge with context: {grant_notification_context}")
+                        send_email_template(
+                            f"Please update grant info for HPC project {project.title}",
+                            "email/request_to_add_grants.txt",
+                            grant_notification_context,
+                            EMAIL_SENDER,
+                            pi_profile.user.email,
+                        )
+
                     if Publication.objects.filter(project=project).count() == 0:
                         publication_notification_context = {
                             "pi_first_name": pi_profile.user.first_name,
                             "project": project.title,
                             "signature": EMAIL_SIGNATURE,
                         }
-                        logger.info(f"will send email with context: {publication_notification_context}")
+                        logger.info(f"sending publication nudge with context: {publication_notification_context}")
+                        send_email_template(
+                            f"Please update publications resulting from use of HPC project {project.title}",
+                            "email/request_to_add_publications.txt",
+                            publication_notification_context,
+                            EMAIL_SENDER,
+                            pi_profile.user.email,
+                        )
         except Exception:
             logger.exception("Exception occured with traceback:", exc_info=True)
