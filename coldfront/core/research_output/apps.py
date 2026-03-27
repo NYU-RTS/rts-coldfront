30a453ea018848d0037864df01a8d96bb2b1a5ab
Hansvb117 <hansel6@hotmail.com>
2026-03-27 23:31:19 +0100

    Delete coldfront directory

--- coldfront/core/research_output/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResearchOutputConfig(AppConfig):
-    name = "coldfront.core.research_output"
--- coldfront/core/school/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SchoolConfig(AppConfig):
-    name = "coldfront.core.school"
-    verbose_name = "School"
