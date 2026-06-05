c6b82a650fb39970d5f5468b8857e5ce0730dfd1
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-06-05 15:34:31 -0400

    project views/forms: update to match the newer html template
    	modified:   coldfront/core/project/forms.py
    	modified:   coldfront/core/project/views.py

--- coldfront/core/project/forms.py
+++ coldfront/core/project/forms.py
@@ -16,9 +16,7 @@ from coldfront.core.project.models import (
 )
 from coldfront.core.utils.common import import_from_settings
 
-EMAIL_DIRECTOR_PENDING_PROJECT_REVIEW_EMAIL = import_from_settings(
-    "EMAIL_DIRECTOR_PENDING_PROJECT_REVIEW_EMAIL"
-)
+EMAIL_DIRECTOR_PENDING_PROJECT_REVIEW_EMAIL = import_from_settings("EMAIL_DIRECTOR_PENDING_PROJECT_REVIEW_EMAIL")
 EMAIL_ADMIN_LIST = import_from_settings("EMAIL_ADMIN_LIST", [])
 EMAIL_DIRECTOR_EMAIL_ADDRESS = import_from_settings("EMAIL_DIRECTOR_EMAIL_ADDRESS", "")
 
@@ -42,58 +40,17 @@ class ProjectAddUserForm(forms.Form):
     last_name = forms.CharField(max_length=150, required=False, disabled=True)
     email = forms.EmailField(max_length=100, required=False, disabled=True)
     source = forms.CharField(max_length=16, disabled=True)
-    role = forms.ModelChoiceField(
-        queryset=ProjectUserRoleChoice.objects.all(), empty_label=None
-    )
+    role = forms.ModelChoiceField(queryset=ProjectUserRoleChoice.objects.all(), empty_label=None)
     selected = forms.BooleanField(initial=False, required=False)
 
 
 class ProjectAddUsersToAllocationForm(forms.Form):
-    allocation = forms.MultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple(attrs={"checked": "checked"}),
-        required=False,
-    )
-
-    def __init__(self, request_user, project_pk, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        project_obj = get_object_or_404(Project, pk=project_pk)
-
-        allocation_query_set = project_obj.allocation_set.filter(
-            resources__is_allocatable=True,
-            is_locked=False,
-            status__name__in=[
-                "Active",
-                "New",
-                "Renewal Requested",
-                "Payment Pending",
-                "Payment Requested",
-                "Paid",
-            ],
-        )
-        allocation_choices = [
-            (
-                allocation.id,
-                "%s (%s) %s"
-                % (
-                    allocation.get_parent_resource.name,
-                    allocation.get_parent_resource.resource_type.name,
-                    allocation.description if allocation.description else "",
-                ),
-            )
-            for allocation in allocation_query_set
-        ]
-        allocation_choices_sorted = []
-        allocation_choices_sorted = sorted(
-            allocation_choices, key=lambda x: x[1][0].lower()
-        )
-        allocation_choices.insert(0, ("__select_all__", "Select All"))
-        if allocation_query_set:
-            self.fields["allocation"].choices = allocation_choices_sorted
-            self.fields[
-                "allocation"
-            ].help_text = "<br/>Select allocations to add selected users to."
-        else:
-            self.fields["allocation"].widget = forms.HiddenInput()
+    pk = forms.IntegerField(disabled=True)
+    selected = forms.BooleanField(initial=False, required=False)
+    resource = forms.CharField(max_length=50, disabled=True)
+    details = forms.CharField(max_length=300, disabled=True, required=False)
+    resource_type = forms.CharField(max_length=50, disabled=True)
+    status = forms.CharField(max_length=50, disabled=True)
 
 
 class ProjectRemoveUserForm(forms.Form):
@@ -106,9 +63,7 @@ class ProjectRemoveUserForm(forms.Form):
 
 
 class ProjectUserUpdateForm(forms.Form):
-    role = forms.ModelChoiceField(
-        queryset=ProjectUserRoleChoice.objects.all(), empty_label=None
-    )
+    role = forms.ModelChoiceField(queryset=ProjectUserRoleChoice.objects.all(), empty_label=None)
     enable_notifications = forms.BooleanField(initial=False, required=False)
 
 
@@ -141,9 +96,7 @@ class ProjectReviewForm(forms.Form):
 
         if project_obj.publication_set.exists():
             latest_publication = project_obj.publication_set.order_by("-created")[0]
-            publication_updated_in_last_year = (
-                now - latest_publication.created
-            ).days < 365
+            publication_updated_in_last_year = (now - latest_publication.created).days < 365
         else:
             publication_updated_in_last_year = None
 
@@ -165,9 +118,7 @@ class ProjectReviewEmailForm(forms.Form):
             project_review_obj.project.pi.last_name,
             EMAIL_DIRECTOR_PENDING_PROJECT_REVIEW_EMAIL,
         )
-        self.fields["cc"].initial = ", ".join(
-            [EMAIL_DIRECTOR_EMAIL_ADDRESS] + EMAIL_ADMIN_LIST
-        )
+        self.fields["cc"].initial = ", ".join([EMAIL_DIRECTOR_EMAIL_ADDRESS] + EMAIL_ADMIN_LIST)
 
 
 class ProjectAttributeAddForm(forms.ModelForm):
@@ -181,13 +132,9 @@ class ProjectAttributeAddForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ProjectAttributeAddForm, self).__init__(*args, **kwargs)
         user = (kwargs.get("initial")).get("user")
-        self.fields["proj_attr_type"].queryset = self.fields[
-            "proj_attr_type"
-        ].queryset.order_by(Lower("name"))
+        self.fields["proj_attr_type"].queryset = self.fields["proj_attr_type"].queryset.order_by(Lower("name"))
         if not user.is_superuser:
-            self.fields["proj_attr_type"].queryset = self.fields[
-                "proj_attr_type"
-            ].queryset.filter(is_private=False)
+            self.fields["proj_attr_type"].queryset = self.fields["proj_attr_type"].queryset.filter(is_private=False)
 
 
 class ProjectAttributeDeleteForm(forms.Form):
