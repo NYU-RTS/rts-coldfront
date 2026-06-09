cf18500a41ceba55beb6e8b6ee57f4d1fcd87489
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-06-09 10:47:24 -0400

    everyting from bootstrap5

--- coldfront/static/src/coldfront.ts
+++ coldfront/static/src/coldfront.ts
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import './scss/coldfront.scss';
-import { Alert, Collapse, Dropdown, Modal, Popover, Tooltip } from 'bootstrap';
+import * as bootstrap from 'bootstrap';
 import 'htmx.org';
 import { initDateSelector } from './dateSelector';
 import { initSelect2 } from './select2';
@@ -24,7 +24,7 @@ Object.assign(window, {
   },
   $: jQuery,
   jQuery,
-  bootstrap: { Alert, Collapse, Dropdown, Modal, Popover, Tooltip },
+  bootstrap,
 });
 
 function initDocument(): void {
