a87e3ae9518e9e45082ce72d0e798a0846738797
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2026-06-17 15:19:54 -0400

    minor

--- coldfront/plugins/slurmrest/utils.py
+++ coldfront/plugins/slurmrest/utils.py
@@ -28,12 +28,12 @@ logger = logging.getLogger(__name__)
 
 
 def log_request(request):
-    logger.info(f"Request event hook: {request.method} {request.url} - Waiting for response")
+    logger.debug(f"Request event hook: {request.method} {request.url} - Waiting for response")
 
 
 def log_response(response):
     request = response.request
-    logger.info(f"Response event hook: {request.method} {request.url} - Status {response.status_code}")
+    logger.debug(f"Response event hook: {request.method} {request.url} - Status {response.status_code}")
 
 
 class SlurmCluster:
